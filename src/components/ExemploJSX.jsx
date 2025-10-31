44ca0b319e5a1a62037b258f9f277c715e64a8b8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-30 21:32:24 -0300

    Adiciona componente inicial ExemploJSX com título CardioLife

---
+++ src/components/ExemploJSX.jsx
@@ -0,0 +1,11 @@
+export default function ExemploJSX() {
+  const titulo = "CardioLife";
+  const descricao = "Aplicação inicial para estudo de ReactJS e medicina";
+
+  return (
+    <section>
+      <h1>Olá, {titulo}!</h1>
+      <p>{descricao}</p>
+    </section>
+  );
+}
